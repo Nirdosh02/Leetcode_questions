7dabd5cd7afa12b0184cfc78c9ff23934a460b6e
Nirdosh02 <141449616+Nirdosh02@users.noreply.github.com>
2024-10-09 00:17:37 +0530

    Time: 0 ms (100%), Space: 10.4 MB (81.12%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& v) {
         int n = v.size();
-        for(int i = n-1; i >= 0; i--){
-            if(i == n-1)
-                v[i]++;
-            if(v[i] == 10){
+        for (int i = n - 1; i >= 0; i--) {
+            if (v[i] == 9) {
                 v[i] = 0;
-                if(i != 0){
-                    v[i-1]++;
-                }
-                else{
-                    v.push_back(0);
-                    v[i] = 1;
-                }
+            } else {
+                v[i]++;
+                return v;
             }
         }
+        // If all digits are 9, add a new digit 1 at the beginning
+        v.insert(v.begin(), 1);
         return v;
     }
 };
