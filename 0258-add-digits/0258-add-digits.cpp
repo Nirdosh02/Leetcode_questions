c920b709a4c68f1e3311d8c3e0d370d16bd896b0
Nirdosh02 <141449616+Nirdosh02@users.noreply.github.com>
2024-11-20 22:35:02 +0530

    Time: 4 ms (6.41%), Space: 8.4 MB (15.96%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        if(num == 0) return 0;
+        if(num%9==0){
+            return 9;}
+            else{ return num%9;}
+        
+    }
+};
